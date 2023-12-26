3dab99f869b1530e627b794394ab052983980138
thatchermain <dominikkrzysztalowicz@gmail.com>
2023-12-26 20:20:25 +0100

    Raw final

--- src/components/Results.jsx
+++ src/components/Results.jsx
@@ -3,6 +3,7 @@ import './Results.scss';
 
 const Results = () => {
   const [data, setData] = useState();
+  const [search, setSearch] = useState('');
 
   const results = async () => {
     const res = await fetch(
@@ -11,7 +12,14 @@ const Results = () => {
     const fetchedData = await res.json(res);
     await setData(fetchedData);
     console.log(fetchedData);
+    console.log(regions);
   };
+
+  let regions = [];
+  data &&
+    data.map((item) => {
+      regions.push(item.region);
+    });
 
   return (
     <div className='container'>
@@ -21,48 +29,69 @@ const Results = () => {
           <button className='button' onClick={results}>
             Pobierz wyniki
           </button>
+          {data && (
+            <select
+              className='button'
+              type='select'
+              placeholder='Search'
+              onChange={(e) => setSearch(e.target.value)}
+            >
+              <option value=''>Wybierz region</option>
+              {regions.map((region, index) => {
+                return (
+                  <option key={index} value={region.toLowerCase()}>
+                    {region}
+                  </option>
+                );
+              })}
+            </select>
+          )}
         </div>
         <br />
-        {data &&
-          data.map((item) => (
-            <div className='wrapper'>
-              <table className='table' border={0}>
-                <tr className='table__row'>
-                  <th className='table__row table__row__head'>
-                    Imię i nazwisko
-                  </th>
-                  <th className='table__row table__row__head'>Region</th>
-                  <th className='table__row table__row__head'>Odpowiedzi</th>
-                  <th className='table__row table__row__head'>Poprawne</th>
-                  <th className='table__row table__row__head'>Błęde</th>
-                  <th className='table__row table__row__head'>Wynik</th>
-                </tr>
-                <br></br>
-                <tbody className='table__body'>
-                  <tr className='table__body table__body__row'>
-                    <td className='table__body__row table__body__row__data'>
-                      {item.user}
-                    </td>
-                    <td className='table__body__row table__body__row__data'>
-                      {item.region}
-                    </td>
-                    <td className='table__body__row table__body__row__data'>
-                      {item.numberOfQuestionsAnswered}
-                    </td>
-                    <td className='table__body__row table__body__row__data'>
-                      {item.correctAnswer}
-                    </td>
-                    <td className='table__body__row table__body__row__data'>
-                      {item.wrongAnswer}
-                    </td>
-                    <td className='table__body__row table__body__row__data'>
-                      {item.score + ' %'}
-                    </td>
-                  </tr>
-                </tbody>
-              </table>
-            </div>
-          ))}
+        <div className='wrapper'>
+          <table className='table' border={0}>
+            <tr className='table__row'>
+              <th className='table__row table__row__head'>Imię i nazwisko</th>
+              <th className='table__row table__row__head'>Region</th>
+              <th className='table__row table__row__head'>Odpowiedzi</th>
+              <th className='table__row table__row__head'>Poprawne</th>
+              <th className='table__row table__row__head'>Błęde</th>
+              <th className='table__row table__row__head'>Wynik</th>
+            </tr>
+            <br></br>
+            <tbody className='table__body'>
+              {data &&
+                data
+                  .filter((entry) => {
+                    return search.toLocaleLowerCase() === ''
+                      ? entry
+                      : entry.region.toLocaleLowerCase().includes(search);
+                  })
+                  .map((item) => (
+                    <tr className='table__body table__body__row'>
+                      <td className='table__body__row table__body__row__data'>
+                        {item.user}
+                      </td>
+                      <td className='table__body__row table__body__row__data'>
+                        {item.region}
+                      </td>
+                      <td className='table__body__row table__body__row__data'>
+                        {item.numberOfQuestionsAnswered}
+                      </td>
+                      <td className='table__body__row table__body__row__data'>
+                        {item.correctAnswer}
+                      </td>
+                      <td className='table__body__row table__body__row__data'>
+                        {item.wrongAnswer}
+                      </td>
+                      <td className='table__body__row table__body__row__data'>
+                        {item.score + ' %'}
+                      </td>
+                    </tr>
+                  ))}
+            </tbody>
+          </table>
+        </div>
       </>
     </div>
   );
